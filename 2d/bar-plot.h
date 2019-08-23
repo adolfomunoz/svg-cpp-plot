fea110848488c9961b95667893b2ba8e4eb2a3c3
adolfomunoz <adolfo@unizar.es>
2019-08-23 16:13:35 +0200

    Solved bar plot.

--- 2d/bar-plot.h
+++ 2d/bar-plot.h
@@ -8,10 +8,11 @@ namespace _2d {
 
 template<typename T, typename = std::enable_if_t<std::is_floating_point_v<T> > >
 polygon bar_plot(const std::vector<T>& data) {
-	float dx = data.size();
+	float dx = 1.0f/float(data.size());
 	polygon b({{0.0f,0.0f}});
-	for (std::size_t i = 0;i<data.size();++i)
-		b.add_points({{float(i)*dx,float(data[0])},{float(i+1)*dx,float(data[0])}});
+	for (std::size_t i = 0;i<data.size();++i) {
+		b.add_points({{float(i)*dx,float(data[i])},{float(i+1)*dx,float(data[i])}});
+	}
 	b.add_points({{1.0f,0.0f}});
 	return b;
 }	
