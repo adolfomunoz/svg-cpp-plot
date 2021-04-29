16bedc67c7ec55b9d0920d884d983333b7e085c6
adolfomunoz <adolfo@unizar.es>
2021-04-29 17:45:47 +0200

    Added new example - nicest fruit.

---
+++ main/examples/nicest-fruit/nicest-fruit.cc
@@ -0,0 +1,8 @@
+#include "../../../svg-cpp-plot.h"
+
+int main() {
+    svg_cpp_plot::SVGPlot plt;
+    plt.bar({"Apple","Orange","Banana","Kiwifruit","Blueberry","Grapes"},{35,30,9,26,39,4})
+         .color({"red","orange","yellow","green","cyan","purple"});
+    plt.savefig("nicest-fruit.svg");
+}    
